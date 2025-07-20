e1b6ece5673e5d89be999481d7f0c13f2fa4d719
NI57721 <NI57721@users.noreply.github.com>
2025-07-20 18:09:40 +0900

    Add functions

---
+++ functions/steam-play.fish
@@ -0,0 +1,71 @@
+#!/usr/bin/env fish
+
+function steam-play
+  argparse 'f/fetch' 'h/help' -- $argv
+    or return 1
+  set --local arg (string join ' ' -- $argv)
+
+  if set --query _flag_help
+    __steam_play_help
+    return 0
+  end
+
+  if set --query _flag_fetch
+    __steam_play_fetch
+    __steam_play_cache
+    return 0
+  end
+
+  if test -z "$arg"
+    printf 'The id or title of a game is required.' >&2
+    return 1
+  end
+
+  steam "steam://rungameid/$arg"
+end
+
+function __steam_play_state_dir
+  if set --query STEAM_PLAY_STATE
+    printf "$STEAM_PLAY_STATE"
+  else
+    printf "$XDG_STATE_HOME/steam-play"
+  end
+end
+
+function __steam_play_fetch
+  set --local STEAM_API_URI https://api.steampowered.com/ISteamApps/GetAppList/v0002/
+  set --local path (__steam_play_state_dir)
+
+  mkdir --parent "$path"
+  curl --silent "$STEAM_API_URI" > "$path/app-list.json"
+end
+
+function __steam_play_cache
+  set --local path (__steam_play_state_dir)
+  set --local cache "$path/cache"
+
+  if test -f "$path/app-list.json"
+    jq -rc '.applist.apps[] | "\(.appid)\\t\(.name)" ' "$path/app-list.json" > "$cache"
+  end
+end
+
+function __steam_play_help
+  printf "Usage:
+  steam-play [options] <id|title>
+
+Launch a Steam game specified by its app ID.
+Completion by ID or title is available after fetching the app list by -f/--fetch
+option.
+Note: Completion is not triggered until at least 3 characters are typed, to
+avoid hanging due to the large number of entries.
+
+Options:
+  -f, --fetch           Fetch the app list of games.
+  -h, --help            Display this help message.
+
+Environment Variable:
+  STEAM_PLAY_STATE      Path to store or read the app list.
+                        Defaults to: \$XDG_STATE_HOME/steam-play
+"
+end
+
